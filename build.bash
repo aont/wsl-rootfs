bdbf839bd98e9d2933ddb35fd0932bddf9a93c56
aont <220471+aont@users.noreply.github.com>
2025-12-13 17:52:50 +0900

    Add build script for OCI image handling

---
+++ build.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -xe
+
+IMAGENAME="$1"
+TAG="$2"
+
+if [[ -z "$IMAGENAME" || -z "$TAG" ]]; then
+  echo "usage: $0 imagename tag"
+  exit 1
+fi
+
+OCIPATH=~/.oci
+ROOTFSPATH=~/.rootfs
+IMAGEPATH=~/image
+
+mkdir -p "$OCIPATH"
+mkdir -p "$ROOTFSPATH"
+mkdir -p "$IMAGEPATH"
+
+DIGEST_LIST=$(skopeo inspect --raw docker://"$IMAGENAME":"$TAG" | jq -r '.manifests[] | select(.platform.architecture=="amd64") | .digest')
+NUM_DIGESTS="$(echo "$DIGEST_LIST" | wc -l)"
+
+if [[ ! "$NUM_DIGESTS" -eq 1 ]]; then
+    echo "number of image is ${NUM_DIGESTS}"
+    exit 1
+fi
+
+DIGEST_SHORT="${DIGEST_LIST#sha256:}"
+DIGEST_SHORT="${DIGEST_SHORT:0:7}"
+
+skopeo copy docker://"$IMAGENAME":"$TAG" oci:"$OCIPATH"/"$IMAGENAME":"$TAG"
+umoci raw unpack --image "$OCIPATH"/"$IMAGENAME":"$TAG" "$ROOTFSPATH"
+tar czf "$IMAGEPATH"/"$IMAGENAME"_"$TAG"_"$DIGEST_SHORT".tar.gz --numeric-owner --xattrs --acls --selinux -p -C "$ROOTFSPATH" .
